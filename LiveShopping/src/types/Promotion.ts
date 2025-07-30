88d6f682e0ae9f39bbf69b897ec6c27eb083e5b3
AnthonyClaran <157488965+AnthonyClaran@users.noreply.github.com>
2025-07-30 13:56:11 +0300

    delete project

--- LiveShopping/src/types/Promotion.ts
+++
@@ -1,9 +0,0 @@
-export type Promotion = {
-  id_promotion: number;
-  name_promotion: string;
-  description?: string;
-  percentage: number;
-  start_date: string;
-  end_date?: string;
-  id_item: number;
-};
--- LiveShopping/src/types/Sale.ts
+++
@@ -1,6 +0,0 @@
-export type Sale = {
-  id_sale: number;
-  sale_date: string;
-  is_paid?: boolean;
-  id_commande: number;
-};

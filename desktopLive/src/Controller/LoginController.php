e26c0ccad8775036345dd4a33578fe1cb727edf4
AnthonyClaran <157488965+AnthonyClaran@users.noreply.github.com>
2025-08-11 10:52:37 +0300

    update user

--- desktopLive/src/Controller/LoginController.php
+++ desktopLive/src/Controller/LoginController.php
@@ -89,6 +89,11 @@ final class LoginController extends AbstractController
             'user' => [
                 'id_user' => $user->getId(),
                 'username' => $user->getUsername(),
+                'email' => $user->getEmail(),
+                'contact' => $user->getContact(),
+                'address' => $user->getAddress(),
+                'country' => $user->getCountry(),
+                'image' => $user->getImages(),
                 'is_seller' => $user->isSeller(),
             ]
         ]);
